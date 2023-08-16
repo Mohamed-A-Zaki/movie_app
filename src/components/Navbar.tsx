97724f0b05125102aaa2d0f8857e5a3abaf5ebbf
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2023-08-16 23:31:45 +0300

    upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,12 @@
-import logo from "../images/logo.png";
-import SearchField from "./SearchField";
-import { AppBar, Box, Container, Toolbar } from "@mui/material";
+import NavbarContent from "./NavbarContent";
+import { AppBar, Container, Toolbar } from "@mui/material";
 
 const Navbar = () => {
   return (
     <AppBar sx={{ bgcolor: "#b45b35", p: 1, position: "static" }}>
       <Container fixed>
         <Toolbar sx={{ gap: 3, alignItems: "center" }}>
-          <Box component={"img"} src={logo} width={60}></Box>
-          <SearchField />
+          <NavbarContent />
         </Toolbar>
       </Container>
     </AppBar>
